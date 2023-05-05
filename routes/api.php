7da375a0815c86978fbf2719a038508f7161a332
thinesjs <thinesjs@gmail.com>
2023-05-05 16:42:07 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RiotAPIController;
 use App\Http\Controllers\APIController;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
